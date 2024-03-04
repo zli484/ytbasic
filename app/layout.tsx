312fc8df3083a21a8f999a9b157f719160504b23
zhaoqi li <zli484@wisc.edu>
2024-03-04 15:21:58 -0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import icon from "@/public/favicon.png";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
           {/* <HeaderSideNav /> */}
           {children}
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
